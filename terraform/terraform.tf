cc96845de3d6a2c1fa80a5dbebd88a2c451a47b9
Theo Vermorel <theo.vermorel@epitech.eu>
2025-12-12 17:32:38 +0100

    fix: helm provider version

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    helm = {
+      source  = "hashicorp/terraform-provider-helm"
+      version = "3.1.1"
+    }
   }
 
   required_version = ">= 1.14"
