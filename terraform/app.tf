9a8909feed85d6c472d9bf8981d051f0da57ab93
Nicolas Salimina <nicolas.salimina@epitech.eu>
2025-12-19 12:25:54 +0100

    ADD force update and clean

--- terraform/app.tf
+++ terraform/app.tf
@@ -13,6 +13,8 @@ resource "helm_release" "app" {
   timeout       = 900
   wait_for_jobs = true
   atomic        = false
+  cleanup_on_fail = true
+  force_update = true
 
   depends_on = [
     data.aws_eks_cluster.cluster,
