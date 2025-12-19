6b65a189f05d5c0e2b3a4a4fc00971f489d311cf
Nicolas Salimina <nicolas.salimina@epitech.eu>
2025-12-19 11:53:44 +0100

    FIX app.tf

---
+++ terraform/app.tf
@@ -0,0 +1,21 @@
+resource "helm_release" "app" {
+  name      = var.helm_release_name
+  chart     = var.helm_chart_path
+  namespace = var.kubernetes_app_namespace
+
+  create_namespace = true
+
+  values = [
+    file(var.helm_values_path)
+  ]
+
+  wait                  = true
+  timeout               = 900
+  wait_for_jobs         = true
+  atomic                = false
+
+  depends_on = [
+    data.aws_eks_cluster.cluster,
+    data.aws_eks_cluster_auth.cluster
+  ]
+}
--- terraform/helm.tf
+++ terraform/helm.tf
@@ -16,24 +16,3 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "app" {
-  name      = var.helm_release_name
-  chart     = var.helm_chart_path
-  namespace = var.kubernetes_app_namespace
-
-  create_namespace = true
-
-  values = [
-    file(var.helm_values_path)
-  ]
-
-  wait                  = true
-  timeout               = 900
-  wait_for_jobs         = true
-  atomic                = false
-
-  depends_on = [
-    data.aws_eks_cluster.cluster,
-    data.aws_eks_cluster_auth.cluster
-  ]
-}
