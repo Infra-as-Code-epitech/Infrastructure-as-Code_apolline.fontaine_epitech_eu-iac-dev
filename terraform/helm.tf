9d67f92758729240f6e65ca25b4365f1bb77d494
Nicolas Salimina <nicolas.salimina@epitech.eu>
2025-12-19 11:37:15 +0100

    FIX timeout helm_release app

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -27,6 +27,13 @@ resource "helm_release" "app" {
     file(var.helm_values_path)
   ]
 
-  wait    = true
-  timeout = 300
+  wait                  = true
+  timeout               = 900
+  wait_for_jobs         = true
+  atomic                = false
+
+  depends_on = [
+    data.aws_eks_cluster.cluster,
+    data.aws_eks_cluster_auth.cluster
+  ]
 }
