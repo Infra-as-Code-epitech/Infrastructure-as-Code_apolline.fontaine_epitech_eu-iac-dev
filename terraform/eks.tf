bd20d71adbf60ce852e4e9b6410c1ce547491311
Theo Vermorel <theo.vermorel@epitech.eu>
2025-12-19 11:30:10 +0100

    add: t3.small app node group instance type + remove helm app

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -23,7 +23,7 @@ module "eks-managed-node-group" {
       }
     },
     runners_node_group = {
-      instance_types = ["c7i-flex.large"]
+      instance_types = ["t3.small"]
       ami_type       = "AL2023_x86_64_STANDARD"
       min_size       = 1
       max_size       = 3
