47e28577b7172b4ccf19757736bdef6fa8f69b54
Theo Vermorel <theo.vermorel@epitech.eu>
2025-12-19 14:24:22 +0100

    fix: entry_acces

--- terraform/entry_access.tf
+++ terraform/entry_access.tf
@@ -18,14 +18,15 @@ resource "aws_eks_access_entry" "admins" {
 }
 
 resource "aws_eks_access_policy_association" "admin_policy" {
-  for_each = aws_eks_access_entry.admins
+  for_each = toset(local.admin_users)
 
   cluster_name  = module.eks-managed-node-group.cluster_name
-  principal_arn = each.value.principal_arn
+  principal_arn = each.value
 
   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
     type = "cluster"
   }
+  depends_on = [aws_eks_access_entry.admins]
 }
