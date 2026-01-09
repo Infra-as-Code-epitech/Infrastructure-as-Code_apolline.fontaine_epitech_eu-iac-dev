b6f6c2559361a109c4e1b96e69ed221109b17fdf
Theo Vermorel <theo.vermorel@epitech.eu>
2026-01-09 15:33:10 +0100

    add: rds networking

--- terraform/infra/rds.tf
+++ terraform/infra/rds.tf
@@ -14,6 +14,24 @@ resource "aws_secretsmanager_secret_version" "app_config_val" {
   })
 }
 
+resource "aws_db_subnet_group" "rds" {
+  name       = "rds"
+  subnet_ids = module.vpc.private_subnets
+  tags       = var.tags
+}
+
+resource "aws_security_group" "rds" {
+  name_prefix = "rds-sg"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.rds.id
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+}
+
 resource "aws_db_instance" "rds" {
   region                              = var.region
   allocated_storage                   = 20
@@ -21,6 +39,8 @@ resource "aws_db_instance" "rds" {
   engine                              = "postgres"
   engine_version                      = "17.6"
   instance_class                      = "db.t4g.micro"
+  db_subnet_group_name                = aws_db_subnet_group.rds.name
+  vpc_security_group_ids              = module.vpc.default_security_group_id
   manage_master_user_password         = true
   iam_database_authentication_enabled = true
   apply_immediately                   = true
