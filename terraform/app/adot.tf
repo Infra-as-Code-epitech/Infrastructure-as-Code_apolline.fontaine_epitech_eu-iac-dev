46c0a2e0417e52c9f7ddae96cf569d03b76d8eba
Theo Vermorel <theo.vermorel@epitech.eu>
2026-01-14 14:33:00 +0100

    add: cluster role binding for observability service account

--- terraform/app/adot.tf
+++ terraform/app/adot.tf
@@ -12,6 +12,45 @@ resource "kubernetes_service_account_v1" "adot" {
   depends_on = [kubernetes_namespace_v1.observability]
 }
 
+resource "kubernetes_cluster_role_v1" "adot_permissions" {
+  metadata {
+    name = "adot-collector-role"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods", "nodes", "namespaces", "configmaps"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["nodes/stats", "nodes/proxy"]
+    verbs      = ["get", "list", "watch"]
+  }
+
+  depends_on = [kubernetes_service_account_v1.adot]
+}
+
+resource "kubernetes_cluster_role_binding_v1" "adot_binding" {
+  metadata {
+    name = "adot-collector-binding"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role_v1.adot_permissions.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "observability-sa"
+    namespace = "observability"
+  }
+
+  depends_on = [kubernetes_service_account_v1.adot]
+}
+
 resource "kubernetes_manifest" "adot_collector" {
   computed_fields = ["spec.config"]
   manifest = {
